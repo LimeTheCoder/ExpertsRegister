e89261aff8b5ba369f1ff815806bb00a13a55b36
LimeTheCoder <limethecoder@gmail.com>
2016-06-01 20:56:21 +0300

    Add main page

--- register/views.py
+++ register/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.template.loader import get_template
 from django.template import Context
 from django.http import HttpResponse
+from .models import Expert
 
 def main(request):
-	return render(request, 'register/main_page.html')
+	expert_lst = Expert.objects.all()
+
+	context = {'experts' : expert_lst}
+
+	return render(request, 'register/main_page.html', context)
