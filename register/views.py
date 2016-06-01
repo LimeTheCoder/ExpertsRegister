705a4b605f2fc89f46731a1ab0bad2c3279d5b68
LimeTheCoder <limethecoder@gmail.com>
2016-06-01 21:43:45 +0300

    Add validation page

--- register/views.py
+++ register/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.template.loader import get_template
 from django.template import Context
 from django.http import HttpResponse
-from .models import Expert
+from .models import Expert, Validation
 
 def main(request):
 	expert_lst = Expert.objects.all()
@@ -11,3 +11,9 @@ def main(request):
 	context = {'experts' : expert_lst}
 
 	return render(request, 'register/main_page.html', context)
+
+def validation(request, expert_id):
+	expert = Expert.objects.get(id=expert_id)
+	validations = Validation.objects.filter(expert=expert)
+	context = {'validations' : validations, 'organization' : expert.organization}
+	return render(request, 'register/validation_page.html', context)
